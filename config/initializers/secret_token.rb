076797d6249f3272bbd8e4ca10242f20cf6fbaee
Patrycja Urban <patrycja.urban@gmail.com>
2010-12-15 17:13:41 +0100

    it works!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fejsik::Application.config.secret_token = '90d2f0754c895527912946378c456d087ea613fc4e5aeaaf746b2778936d729735873b152c471addf5125e71a9ddd3a5f4823fc5fca9937781abc77c2d0c2290'
